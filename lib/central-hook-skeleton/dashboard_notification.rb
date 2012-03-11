1b8adced3169b32cc837a682d8b79e74106fc582
Aaron Daniel <amdtech@me.com>
2012-03-11 16:45:56 -0700

    Move to match convention

--- lib/central-hook-skeleton/dashboard_notification.rb
+++ lib/central-hook-skeleton/dashboard_notification.rb
@@ -1,9 +1,9 @@
 # This class sets up a "Hello World!" output on the dashboard
 
-module HelloWorld
+module CentralHookSkeleton
   def self.dashboard_notification
     "Hello World!"
   end
 end
 
-Central.register_hook(:dashboard_notification, HelloWorld)
+Central.register_hook(:dashboard_notification, CentralHookSkeleton)
